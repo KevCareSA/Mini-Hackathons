81039f0966ffb4b9315528156c8e60a0f64db331
KevCareSA <kev.mkbn2@gmail.com>
2026-01-16 02:47:42 +0200

    feat: add sidebar toggle and active tab state in App

--- DashBoard/src/App.jsx
+++ DashBoard/src/App.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import Sidebar from './components/Sidebar'
 
 function App() {
+  const [sidebar, setSidebarOpen] = useState(false);
+  const [activeTab, setActiveTab] = useState('overview');
+
   return (
     <div className="min-h-screen bg-gradient-to-br from-indigo-900 via-purple-900 
     to-pink-900 relative overflow-hidden">
       <div className='flex min-h-screen relative z-10'>
-         <Sidebar />
+         <Sidebar 
+         sidebar={sidebar} 
+         setSidebarOpen={setSidebarOpen}
+         activeTab={activeTab}
+          setActiveTab={setActiveTab}
+          />
       </div>
     </div>
   );
