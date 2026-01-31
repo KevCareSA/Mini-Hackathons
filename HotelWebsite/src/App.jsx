dc62ef70712f76b4caabdda33915d92b2f18b551
KevCareSA <kev.mkbn2@gmail.com>
2026-01-31 23:02:25 +0200

    feat: adding conditional Navbar display based on URL path using useLocation

--- HotelWebsite/src/App.jsx
+++ HotelWebsite/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { useLocation } from 'react-router-dom';
 
 function App() {
+
+  const isOwnerPath = useLocation().pathname.includes('/owner');
+
   return (
-    <div>App</div>
+    <div>
+        <div className='min-h-screen bg-gradient-to-br from-white-100 to-gray-400 relative overflow-hidden'>
+           {!isOwnerPath && <Navbar /> }
+        </div>
+    </div>
   )
 }
 
