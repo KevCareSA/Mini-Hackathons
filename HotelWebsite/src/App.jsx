a5539ad0588313ac301d44169719ff5cb3883735
KevCareSA <kev.mkbn2@gmail.com>
2026-02-06 21:32:48 +0200

    feat: add React Router routes and Home page to App.jsx

--- HotelWebsite/src/App.jsx
+++ HotelWebsite/src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
 
   const isOwnerPath = useLocation().pathname.includes('/owner');
 
   return (
-        <div className='min-h-screen bg-gradient-to-br from-white to-gray-400 relative overflow-hidden'>
+
+        <div className='min-h-screen bg-gradient-to-r from-white to-gray-400 relative overflow-hidden'>
            {!isOwnerPath && <Navbar /> }
+           <div className='min-h-[70vh]'>
+            <Routes>
+              <Route path='/' element={<Home />} />     
+            </Routes>
+           </div>
         </div>
   )
 }
 
 export default App
+0
