fb9632732a8d7848b5cf6d6ebb19b08f18415f66
KevCareSA <kev.mkbn2@gmail.com>
2026-02-08 21:41:23 +0200

    feat(navbar): add BookingIcon component and integrate Clerk user state with routing

--- HotelWebsite/src/components/Navbar.jsx
+++ HotelWebsite/src/components/Navbar.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { assets } from '../assets/assets';
-import { useClerk, useUser } from '@clerk/clerk-react';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
+const BookingIcon = () => (
+    <svg
+    className="w-4 h-4 text-gray-700"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    fill="none"
+    viewBox="0 0 24 24"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4"
+    />
+  </svg>
+);
 
 function Navbar() {
 
@@ -19,7 +38,9 @@ function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const { openSignIn } = useClerk();
-    const { user, isSignedIn } = useUser();
+    const { user, isSignedIn } = useUser(); 
+    const navigate = useNavigate();
+    const location = useLocation();
 
     useEffect(() => {
         const handleScroll = () => {
@@ -53,6 +74,22 @@ function Navbar() {
                 {/* Desktop Right */}
                 <div className="hidden md:flex items-center gap-4">
                     <img src={assets.searchIcon} alt="search" className={`${isScrolled && "invert"} h-7 transition-all duration-500`} />
+                    
+                   { user ?
+                   <UserButton>
+                    <UserButton.MenuItems>
+                        <UserButton.Action 
+                            label='My Bookings' 
+                            labelIcon={<BookingIcon />} onClick={()=> navigate('/my-bookings')} />
+                    </UserButton.MenuItems>
+                   </UserButton> 
+                   :
+                   (<button onClick={openSignIn} className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-white text-black"}`}>
+                        Login
+                    </button>)
+                   
+                } 
+
                     <button onClick={openSignIn} className={`px-8 py-2.5 rounded-full ml-4 transition-all duration-500 ${isScrolled ? "text-white bg-black" : "bg-white text-black"}`}>
                         Login
                     </button>
