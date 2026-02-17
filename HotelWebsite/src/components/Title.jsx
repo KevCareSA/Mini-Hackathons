ad355fa71e6b46dedc06fe271d06973db2c1b17d
KevCareSA <kev.mkbn2@gmail.com>
2026-02-17 21:40:18 +0200

    feat(title): add new reusable Title component with alignment, font, and subtitle support

---
+++ HotelWebsite/src/components/Title.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Title({title, subTitle, align, font}) {
+  return (
+    <div className={`flex flex-col justify-center items-center text-center gap-2 
+        ${align === 'left' && 'md:items-start md:text-left'}`}>
+        <h1 className={`text-4xl md:text-[40px] ${font || 'font-playfair'}`}>
+            {title}
+        </h1>
+        <p className='text-sm md:text-base text-gray-700/90 mt-2 max-w-174'>
+            {subTitle}
+        </p>
+    
+    </div>
+  )
+}
+
+export default Title
