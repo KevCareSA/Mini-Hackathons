aa174646d16e8c050184bb47fa78cdae1e871ae4
KevCareSA <kev.mkbn2@gmail.com>
2026-02-19 23:12:45 +0200

    style(exclusive-offers): update layout to use responsive grid for offer cards

--- HotelWebsite/src/components/ExclusiveOffers.jsx
+++ HotelWebsite/src/components/ExclusiveOffers.jsx
@@ -17,7 +17,7 @@ function ExclusiveOffers() {
             </button>
         </div>
 
-        <div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
             {exclusiveOffers.map((item) => (
                 <div key={item._id}
                     className='group relative flex flex-col items-start justify-between
