1997916464c61ed5854cca4da1b9754397670afb
KevCareSA <kev.mkbn2@gmail.com>
2026-04-14 21:28:31 +0200

    feat: add testimonial cards layout

---
+++ HotelWebsite/src/components/Testimonial.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Title from './Title'
+
+function Testimonial() {
+  return (
+    <div>
+        <Title title="What Guests Say" subTitle="Discover why our guests love 
+        staying with us and what makes us different." />
+    </div>
+  )
+}
+
+export default Testimonial
