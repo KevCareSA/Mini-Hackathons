83e967cee6b3bf772728f12ca7dfb448865f79d7
KevCareSA <kev.mkbn2@gmail.com>
2026-04-21 21:50:04 +0200

    fix: adjust BookingIcon SVG path data to fix icon shape

--- HotelWebsite/src/components/Navbar.jsx
+++ HotelWebsite/src/components/Navbar.jsx
@@ -20,7 +20,8 @@ const BookingIcon = () => (
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth="2"
-      d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 0 0 2 2h12M9 3v14m7 0v4"
+      d="M5 19V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v13H7a2 2 0 0 0-2 2Zm0 0a2 2 0 
+         0 0 2 2h12M9 3v14m7 0v4"
     />
   </svg>
 );
